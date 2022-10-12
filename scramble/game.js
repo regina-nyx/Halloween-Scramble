be6efd6777eca7e12db666ce53c6f3e353a3b8b9
reginanyx <112037928+regina-nyx@users.noreply.github.com>
2022-10-12 15:18:20 +0000

    TODO 1 COMPLETE

--- scramble/game.js
+++ scramble/game.js
@@ -1,11 +1,11 @@
 /********************************************************************
-  TODO 1: Practice changing the case of strings
-    - With the first letter of every word capitalized, it makes it too easy to play. When scrambled the word should either be all upper or lower case
+  TODO 1 COMPLETE: Practice changing the case of strings
+    - With the first letter of every word capitalized, it makes it too easy to play. When scrambled the word should either be all upper or lower case 
     - HINT: the variable that hold the word that will be scrambled is called "selectedWord"
 
   TODO 2: JS is case sensitive. -- Remember how I showed  "ken" and "Ken" are not the same.
     - When checking your answer, the comparision is case sensitive. You should make sure that the cases are the same.
-    - To do this, locate where the code is COMPARING the selected word to the value in the form field and set them both to either upper or lower case
+    - To do this, locate where the code is COMPARING the selected word to the value in the form field and set them BOTH to either upper or lower case
 
   TODO 3: Clean up the user input. This is an important step in every script that processes user submitted data.
    - Sometimes users accidently add a space to the end of their words so while they mean to submit the string "pumpkin" they actually submit "pumpkin " -- not the space at the end.
